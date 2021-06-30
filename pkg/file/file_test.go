dde3f238644ba2ceab2dea6ae0c027a806ed6dd7
swelsch <stefan.welsch@b-nova.com>
2021-06-30 14:21:47 +0200

    - test push trigger for tekton

--- pkg/file/file_test.go
+++ pkg/file/file_test.go
@@ -1,11 +1,12 @@
 package file
 
 import (
+	"fmt"
 	"testing"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
 func TestPathExists(t *testing.T) {
-
+	fmt.Println("test")
 }
